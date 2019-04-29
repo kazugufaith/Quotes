009773ac8957e6ab6c7590ede5df261360c7c41a
Okall Vivian <vivieokall@gmail.com>
2019-04-29 09:43:07 +0300

    Added imports and metadata in qoutes.ts

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,10 +1,10 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Quote } from '../quote';
-// @Component({
-//   selector: 'app-quotes',
-//   templateUrl: './quotes.component.html',
-//   styleUrls: ['./quotes.component.css']
-// })
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
 // export class QuotesComponent implements OnInit {
 
 //   quotes = [
