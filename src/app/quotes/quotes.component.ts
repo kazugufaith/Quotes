5de531e32979110ed7cbe501e63d4adbfd99076d
Okall Vivian <vivieokall@gmail.com>
2019-04-29 09:46:32 +0300

    Added new quote function, plus vote and downvote

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -24,32 +24,32 @@ export class QuotesComponent implements OnInit {
     }
   }
 
-//   addNewQuote(quote) {
-//     if (quote.enterQuote == "" || quote.author == "" || quote.submitter == "") {
-//       alert("Empty field");
-//     }
-//     else {
-//       let quoteLength = this.quotes.length;
-//       quote.id = quoteLength + 1;
-//       quote.submitDate = new Date();
-//       this.quotes.push(quote);
-//     }
-//   }
-//   plusVote(isPlus, index) {
-//     if (isPlus) {
-//       this.quotes[index].upVote += 1;
-//     }
-//   }
+  addNewQuote(quote) {
+    if (quote.enterQuote == "" || quote.author == "" || quote.submitter == "") {
+      alert("Empty field");
+    }
+    else {
+      let quoteLength = this.quotes.length;
+      quote.id = quoteLength + 1;
+      quote.submitDate = new Date();
+      this.quotes.push(quote);
+    }
+  }
+  plusVote(isPlus, index) {
+    if (isPlus) {
+      this.quotes[index].upVote += 1;
+    }
+  }
 
-//   minusVote(isMinus, index) {
-//     if (isMinus) {
-//       this.quotes[index].downVote += 1;
-//     }
-//   }
+  minusVote(isMinus, index) {
+    if (isMinus) {
+      this.quotes[index].downVote += 1;
+    }
+  }
 
-//   constructor() { }
+  constructor() { }
 
 
-//   ngOnInit() {
-//   }
-// }
+  ngOnInit() {
+  }
+}
