87ed2491aca210792840431dc6ce7caca48c56c9
Okall Vivian <vivieokall@gmail.com>
2019-04-29 09:52:58 +0300

    Added Component annotation and metadata to details.component.ts

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,10 +1,10 @@
-// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-// import { Quote } from '../quote';
-// @Component({
-//   selector: 'app-details',
-//   templateUrl: './details.component.html',
-//   styleUrls: ['./details.component.css']
-// })
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
 // export class DetailsComponent implements OnInit {
 //   @Input() quote:Quote;
 
