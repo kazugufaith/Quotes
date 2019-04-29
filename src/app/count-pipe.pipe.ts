e48b6a9a7e2404b72629eca92a4a6e8154597dc4
Okall Vivian <vivieokall@gmail.com>
2019-04-29 10:02:55 +0300

    Added count-pipe.pipe.ts

--- src/app/count-pipe.pipe.ts
+++ src/app/count-pipe.pipe.ts
@@ -1,33 +1,33 @@
-// import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
-// @Pipe({
-//   name: 'countPipe'
-// })
-// export class CountPipePipe implements PipeTransform {
+@Pipe({
+  name: 'countPipe'
+})
+export class CountPipePipe implements PipeTransform {
 
-//   transform(value: any): any {
-//     let today:any = new Date();
-//     var dateDifference =Math.abs(value-today)
-//     const secondsInADay= 86400;
-//     var dateDifferenceSeconds=dateDifference*0.001;
-//     var days = Math.round(dateDifferenceSeconds/secondsInADay);
-//     dateDifferenceSeconds -=(days*secondsInADay);
-//     var hours = Math.round(dateDifferenceSeconds/(60*24)) ;
-//     dateDifferenceSeconds -=(hours*(60*24));
-//     var minutes = Math.round(dateDifferenceSeconds/60) ;
-//     var seconds = Math.round(dateDifferenceSeconds-(minutes*60)) ;
+  transform(value: any): any {
+    let today:any = new Date();
+    var dateDifference =Math.abs(value-today)
+    const secondsInADay= 86400;
+    var dateDifferenceSeconds=dateDifference*0.001;
+    var days = Math.round(dateDifferenceSeconds/secondsInADay);
+    dateDifferenceSeconds -=(days*secondsInADay);
+    var hours = Math.round(dateDifferenceSeconds/(60*24)) ;
+    dateDifferenceSeconds -=(hours*(60*24));
+    var minutes = Math.round(dateDifferenceSeconds/60) ;
+    var seconds = Math.round(dateDifferenceSeconds-(minutes*60)) ;
 
 
-//     if (days>0) {
-//       return days + " days ";
-//     }
-//     if (hours>0) {
-//       return hours + " hours ";
-//     }
-//     if (minutes>0) {
-//       return minutes + " minutes ";
-//     }
-//     return seconds + " seconds ";
-//   }
+    if (days>0) {
+      return days + " days ";
+    }
+    if (hours>0) {
+      return hours + " hours ";
+    }
+    if (minutes>0) {
+      return minutes + " minutes ";
+    }
+    return seconds + " seconds ";
+  }
 
-// }
+}
